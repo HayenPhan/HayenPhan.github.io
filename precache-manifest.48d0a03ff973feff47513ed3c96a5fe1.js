5e89de49f7f3bbe8008b4e137c90d7975bb8968f
Hayen Phan <hayenphan@outlook.com>
2022-12-16 09:34:35 +0100

    Added files.

---
+++ precache-manifest.48d0a03ff973feff47513ed3c96a5fe1.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb8faa5370ea68924f96eb9d07324ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78140cc01c309965333b",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e00b19c4ca5fb16a70b0",
+    "url": "/static/css/main.93b570ba.chunk.css"
+  },
+  {
+    "revision": "78140cc01c309965333b",
+    "url": "/static/js/2.165afff8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.165afff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00b19c4ca5fb16a70b0",
+    "url": "/static/js/main.c8405393.chunk.js"
+  },
+  {
+    "revision": "04ade1f1785a4090f4fa",
+    "url": "/static/js/runtime-main.7b919fd0.js"
+  },
+  {
+    "revision": "8b891034d92470d9838ec9bf12e48e2b",
+    "url": "/static/media/Music-lobby-screen.8b891034.jpg"
+  },
+  {
+    "revision": "5e66d01dc2fe3f018d3e33cf8e93d932",
+    "url": "/static/media/MusicLobby.5e66d01d.png"
+  },
+  {
+    "revision": "18acd084397b2333c0b728493e4e73e6",
+    "url": "/static/media/MusicRoom.18acd084.png"
+  },
+  {
+    "revision": "f48083bee1678665f3ae8d12d97d7ada",
+    "url": "/static/media/bottom_block--desktop.f48083be.png"
+  },
+  {
+    "revision": "7a1175561abb5a1a737260215aee5bac",
+    "url": "/static/media/bottom_block--mobile.7a117556.png"
+  },
+  {
+    "revision": "8f1c7eea41dd0ce105b32301fab64acf",
+    "url": "/static/media/character.8f1c7eea.png"
+  },
+  {
+    "revision": "14fcd21577fe663eca8d7a88050c98a1",
+    "url": "/static/media/iphone_mockup.14fcd215.png"
+  },
+  {
+    "revision": "3fcee8a4d60451a9e3c87313909dcdd8",
+    "url": "/static/media/macbook_mockup--ILE.3fcee8a4.png"
+  },
+  {
+    "revision": "ff2b318f45de5f43ba3bff8523b33635",
+    "url": "/static/media/musicterr.ff2b318f.png"
+  },
+  {
+    "revision": "50aec63687a31757b9345550990bc370",
+    "url": "/static/media/phase--1.50aec636.png"
+  },
+  {
+    "revision": "a631b511a32a05d546c6a41eda944deb",
+    "url": "/static/media/phase--2.a631b511.png"
+  },
+  {
+    "revision": "832787616f62eabe41fa6801e5288c2a",
+    "url": "/static/media/phase--3.83278761.png"
+  },
+  {
+    "revision": "9193391fb89761d13c6f780c7cf93c3b",
+    "url": "/static/media/phase--4.9193391f.png"
+  },
+  {
+    "revision": "3b893d1c7677ce614d235feb7e8f8536",
+    "url": "/static/media/phase-runner-image.3b893d1c.png"
+  },
+  {
+    "revision": "8789175ee64c13d70483805a1a3e2a66",
+    "url": "/static/media/picture--mobile.8789175e.png"
+  },
+  {
+    "revision": "4bc1d14d709cb6c03fde45882203398f",
+    "url": "/static/media/portfolio-strakzat.4bc1d14d.png"
+  },
+  {
+    "revision": "5a272d78d82338cd97df58506ac87db7",
+    "url": "/static/media/portfoliofigureblownaway.5a272d78.png"
+  },
+  {
+    "revision": "8ec3cd0904546706e232dd6e774bf372",
+    "url": "/static/media/portfoliomusicterra.8ec3cd09.png"
+  },
+  {
+    "revision": "f59c17ad75468bbd51a13ec70528df36",
+    "url": "/static/media/portfoliophaserunner.f59c17ad.png"
+  },
+  {
+    "revision": "5b39e1b8b5a408185b1e18d4e08d043d",
+    "url": "/static/media/right_block--desktop.5b39e1b8.png"
+  },
+  {
+    "revision": "efa4980bcf5d099676c8bc9905818669",
+    "url": "/static/media/slime--mobile.efa4980b.png"
+  },
+  {
+    "revision": "ba1231c9f27183ca7624274d2c529835",
+    "url": "/static/media/slime-specialization--mobile.ba1231c9.png"
+  },
+  {
+    "revision": "36620d876ae1b206476f876c7712e876",
+    "url": "/static/media/strakzat.36620d87.png"
+  },
+  {
+    "revision": "1a551b7e773908d78fa21255d8f4fd0f",
+    "url": "/static/media/tropicana.1a551b7e.jpg"
+  },
+  {
+    "revision": "4435392bcde57c322054fbdf07cf8b73",
+    "url": "/static/media/wave--desktop.4435392b.png"
+  },
+  {
+    "revision": "10e3878c7aa2ffc9cee9b2552069f12f",
+    "url": "/static/media/wireframe--1.10e3878c.png"
+  },
+  {
+    "revision": "ff72fcc9ebdbb71e3107ce1ac2e9e46f",
+    "url": "/static/media/wireframe--2.ff72fcc9.png"
+  },
+  {
+    "revision": "c9f735ce5e8c7f5df8893054604b7052",
+    "url": "/static/media/wireframe--3.c9f735ce.png"
+  },
+  {
+    "revision": "5590c9026bf015b0e2927343dd5d631f",
+    "url": "/static/media/wireframe--4.5590c902.png"
+  },
+  {
+    "revision": "e9ba0d48395aafeb01c71910c380b57b",
+    "url": "/static/media/wokkel-image.e9ba0d48.png"
+  }
+]);
